07fac203fb068638ab0de996d76b2ad51d4af737
V10ACDICT <v10acdict@gmail.com>
2016-03-28 00:04:22 +0900

    First3 commit

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,3 +1,6 @@
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; 4.1 効率的な設定ファイルの作り方と管理方法             ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; load-path を追加する関数を定義
 (defun add-to-load-path (&rest paths)
   (let (path)
@@ -10,3 +13,174 @@
 
 ;; 引数のディレクトリとそのサブディレクトリをload-pathに追加
 (add-to-load-path "elisp" "conf" "public_repos")
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; 4.2 環境に応じた設定の分岐                             ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; P65 CUIとGUIによる分岐
+;; ターミナル以外はツールバー、スクロールバーを非表示
+(when window-system
+  ;; tool-barを非表示
+  (tool-bar-mode 0)
+  ;; scroll-barを非表示
+  (scroll-bar-mode 0))
+
+;; CocoaEmacs以外はメニューバーを非表示
+(unless (eq window-system 'ns)
+  ;; menu-barを非表示
+  (menu-bar-mode 0))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; 5.2 キーバインドの設定                                 ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; P80 C-hをバックスペースにする
+;; 入力されるキーシーケンスを置き換える
+;; ?\C-?はDELのキーシケンス
+(keyboard-translate ?\C-h ?\C-?)
+;;; P79-81 お勧めのキー操作
+;; C-mにnewline-and-indentを割り当てる。
+;; 先ほどとは異なりglobal-set-keyを利用
+(global-set-key (kbd "C-m") 'newline-and-indent)
+;; 折り返しトグルコマンド
+(define-key global-map (kbd "C-c l") 'toggle-truncate-lines)
+;; "C-t" でウィンドウを切り替える。初期値はtranspose-chars
+(define-key global-map (kbd "C-t") 'other-window)
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; 5.3 環境変数の設定                                     ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; P82-83 パスの設定
+(add-to-list 'exec-path "/opt/local/bin")
+(add-to-list 'exec-path "/usr/local/bin")
+(add-to-list 'exec-path "~/bin")
+;;; P85 文字コードを指定する
+(set-language-environment "Japanese")
+(prefer-coding-system 'utf-8)
+;;; P86 ファイル名の扱い
+;; Mac OS Xの場合のファイル名の設定
+(when (eq system-type 'darwin)
+  (require 'ucs-normalize)
+  (set-file-name-coding-system 'utf-8-hfs)
+  (setq locale-coding-system 'utf-8-hfs))
+
+;; Windowsの場合のファイル名の設定
+(when (eq window-system 'w32)
+  (set-file-name-coding-system 'cp932)
+  (setq locale-coding-system 'cp932))
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; 5.4 フレームに関する設定                               ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; P87-89 モードラインに関する設定
+;; カラム番号も表示
+(column-number-mode t)
+;; ファイルサイズを表示
+(size-indication-mode t)
+;; リージョン内の行数と文字数をモードラインに表示する（範囲指定時のみ）
+;; http://d.hatena.ne.jp/sonota88/20110224/1298557375
+(defun count-lines-and-chars ()
+  (if mark-active
+      (format "%d lines,%d chars "
+              (count-lines (region-beginning) (region-end))
+              (- (region-end) (region-beginning)))
+      ;; これだとエコーエリアがチラつく
+      ;;(count-lines-region (region-beginning) (region-end))
+    ""))
+
+(add-to-list 'default-mode-line-format
+             '(:eval (count-lines-and-chars)));;; P87-89 モードラインに関する設定
+;; カラム番号も表示
+(column-number-mode t)
+;; ファイルサイズを表示
+(size-indication-mode t)
+;; 時計を表示（好みに応じてフォーマットを変更可能）
+;; (setq display-time-day-and-date t) ; 曜日・月・日を表示
+;; (setq display-time-24hr-format t) ; 24時表示
+(display-time-mode t)
+;; バッテリー残量を表示
+(display-battery-mode t)
+;; リージョン内の行数と文字数をモードラインに表示する（範囲指定時のみ）
+;; http://d.hatena.ne.jp/sonota88/20110224/1298557375
+(defun count-lines-and-chars ()
+  (if mark-active
+      (format "%d lines,%d chars "
+              (count-lines (region-beginning) (region-end))
+              (- (region-end) (region-beginning)))
+      ;; これだとエコーエリアがチラつく
+      ;;(count-lines-region (region-beginning) (region-end))
+    ""))
+
+(add-to-list 'default-mode-line-format
+             '(:eval (count-lines-and-chars)))
+
+;;; P90 タイトルバーにファイルのフルパスを表示
+(setq frame-title-format "%f")
+;; 行番号を常に表示する
+;; (global-linum-mode t)
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; 5.5インデントの設定                                    ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; P92-94 タブ文字の表示幅
+;; TABの表示幅。初期値は8
+(setq-default tab-width 4)
+;; インデントにタブ文字を使用しない
+(setq-default indent-tabs-mode nil)
+;; php-modeのみタブを利用しない
+;; (add-hook 'php-mode-hook
+;;           '(lambda ()
+;;             (setq indent-tabs-mode nil)))
+
+;; C、C++、JAVA、PHPなどのインデント
+(add-hook 'c-mode-common-hook
+          '(lambda ()
+             (c-set-style "bsd")))
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; 5.6 表示・装飾に関する設定                             ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; P95-96 フェイス
+;; リージョンの背景色を変更
+(set-face-background 'region "darkgreen")
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; 5.6 表示・装飾に関する設定                             ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; P95-96 フェイス
+;; リージョンの背景色を変更
+;; (set-face-background 'region "darkgreen")
+
+;; ▼要拡張機能インストール▼
+;;; P96-97 表示テーマの設定
+;; http://download.savannah.gnu.org/releases/color-theme/color-theme-6.6.0.tar.gz
+(when (require 'color-theme nil t)
+  ;; テーマを読み込むための設定
+  (color-theme-initialize)
+  ;; テーマhoberに変更する
+  (color-theme-gray30))
+
+;;; P97-99 フォントの設定
+;;(when (eq window-system 'ns)
+  ;; asciiフォントをMenloに
+;;  (set-face-attribute 'default nil
+  ;;                    :family "Menlo"
+ ;;                     :height 120)
+  ;; 日本語フォントをヒラギノ明朝 Proに
+;;  (set-fontset-font
+ ;;  nil 'japanese-jisx0208
+   ;; 英語名の場合
+   ;; (font-spec :family "Hiragino Mincho Pro"))
+ ;;  (font-spec :family "ヒラギノ明朝 Pro"))
+  ;; ひらがなとカタカナをモトヤシーダに
+  ;; U+3000-303F	CJKの記号および句読点
+  ;; U+3040-309F	ひらがな
+  ;; U+30A0-30FF	カタカナ
+ ;; (set-fontset-font
+;;   nil '(#x3040 . #x30ff)
+;;   (font-spec :family "NfMotoyaCedar"))
+  ;; フォントの横幅を調節する
+ ;; (setq face-font-rescale-alist
+  ;;      '((".*Menlo.*" . 1.0)
+    ;;      (".*Hiragino_Mincho_Pro.*" . 1.2)
+    ;;      (".*nfmotoyacedar-bold.*" . 1.2)
+   ;;       (".*nfmotoyacedar-medium.*" . 1.2)
+    ;;      ("-cdac$" . 1.3))))
+
+
+  (set-face-attribute 'default nil
+                      :family "M+ 1m"
+                      :height 110)
