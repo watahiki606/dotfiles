ba2b86e93ff9a74246a7bb1055b9393f90143502
V10ACDICT <v10acdict@gmail.com>
2016-03-28 16:36:32 +0900

    4 commit

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -154,33 +154,69 @@
   (color-theme-gray30))
 
 ;;; P97-99 フォントの設定
-;;(when (eq window-system 'ns)
+(when (eq window-system 'ns)
   ;; asciiフォントをMenloに
-;;  (set-face-attribute 'default nil
-  ;;                    :family "Menlo"
- ;;                     :height 120)
+  (set-face-attribute 'default nil
+                      :family "M+ 1m"
+                      :height 120)
   ;; 日本語フォントをヒラギノ明朝 Proに
-;;  (set-fontset-font
- ;;  nil 'japanese-jisx0208
+  (set-fontset-font
+   nil 'japanese-jisx0208
    ;; 英語名の場合
    ;; (font-spec :family "Hiragino Mincho Pro"))
- ;;  (font-spec :family "ヒラギノ明朝 Pro"))
+   (font-spec :family "M+ 1m"))
   ;; ひらがなとカタカナをモトヤシーダに
   ;; U+3000-303F	CJKの記号および句読点
   ;; U+3040-309F	ひらがな
   ;; U+30A0-30FF	カタカナ
- ;; (set-fontset-font
+;;  (set-fontset-font
 ;;   nil '(#x3040 . #x30ff)
 ;;   (font-spec :family "NfMotoyaCedar"))
   ;; フォントの横幅を調節する
- ;; (setq face-font-rescale-alist
-  ;;      '((".*Menlo.*" . 1.0)
-    ;;      (".*Hiragino_Mincho_Pro.*" . 1.2)
-    ;;      (".*nfmotoyacedar-bold.*" . 1.2)
-   ;;       (".*nfmotoyacedar-medium.*" . 1.2)
-    ;;      ("-cdac$" . 1.3))))
+  (setq face-font-rescale-alist
+        '((".*Menlo.*" . 1.0)
+          (".*Hiragino_Mincho_Pro.*" . 1.2)
+;;          (".*nfmotoyacedar-bold.*" . 1.2)
+;;          (".*nfmotoyacedar-medium.*" . 1.2)
+          ("-cdac$" . 1.3))))
+
+(when (eq system-type 'windows-nt)
+  ;; asciiフォントをConsolasに
+  (set-face-attribute 'default nil
+                      :family "Consolas"
+                      :height 120)
+  ;; 日本語フォントをメイリオに
+  (set-fontset-font
+   nil
+   'japanese-jisx0208
+   (font-spec :family "メイリオ"))
+  ;; フォントの横幅を調節する
+  (setq face-font-rescale-alist
+        '((".*Consolas.*" . 1.0)
+          (".*メイリオ.*" . 1.15)
+          ("-cdac$" . 1.3))))
 
 
   (set-face-attribute 'default nil
                       :family "M+ 1m"
-                      :height 110)
+                      :height 120)
+ ;; 日本語フォントをヒラギノ明朝 Proに
+  (set-fontset-font
+   nil 'japanese-jisx0208
+   ;; 英語名の場合
+   ;; (font-spec :family "Hiragino Mincho Pro"))
+   (font-spec :family "M+ 1m"))
+  ;; ひらがなとカタカナをモトヤシーダに
+  ;; U+3000-303F	CJKの記号および句読点
+  ;; U+3040-309F	ひらがな
+;; U+30A0-30FF	カタカナ
+;;  (set-fontset-font
+;;   nil '(#x3040 . #x30ff)
+;;   (font-spec :family "NfMotoyaCedar"))
+  ;; フォントの横幅を調節する
+  (setq face-font-rescale-alist
+        '((".*Menlo.*" . 1.0)
+          (".*Hiragino_Mincho_Pro.*" . 1.2)
+;;          (".*nfmotoyacedar-bold.*" . 1.2)
+;;          (".*nfmotoyacedar-medium.*" . 1.2)
+          ("-cdac$" . 1.3)))
