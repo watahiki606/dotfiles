d62a8be67c423f42ae5cae9c999f09815f0cd4dd
V10ACDICT <v10acdict@gmail.com>
2016-03-30 13:55:44 +0900

    0330

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -10,9 +10,8 @@
         (add-to-list 'load-path default-directory)
         (if (fboundp 'normal-top-level-add-subdirs-to-load-path)
             (normal-top-level-add-subdirs-to-load-path))))))
-
 ;; 引数のディレクトリとそのサブディレクトリをload-pathに追加
-(add-to-load-path "elisp" "conf" "public_repos")
+(add-to-load-path "elisp" "conf" "public_repos" "helm" "helm-descbinds")
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; 4.2 環境に応じた設定の分岐                             ;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -157,34 +156,34 @@
 (when (eq window-system 'ns)
   ;; asciiフォントをMenloに
   (set-face-attribute 'default nil
-                      :family "M+ 1m"
-                      :height 120)
+                      :family "Menlo"
+                      :height 110)
   ;; 日本語フォントをヒラギノ明朝 Proに
   (set-fontset-font
    nil 'japanese-jisx0208
    ;; 英語名の場合
    ;; (font-spec :family "Hiragino Mincho Pro"))
-   (font-spec :family "M+ 1m"))
+   (font-spec :family "ヒラギノ明朝 Pro"))
   ;; ひらがなとカタカナをモトヤシーダに
   ;; U+3000-303F	CJKの記号および句読点
   ;; U+3040-309F	ひらがな
   ;; U+30A0-30FF	カタカナ
-;;  (set-fontset-font
-;;   nil '(#x3040 . #x30ff)
-;;   (font-spec :family "NfMotoyaCedar"))
+  (set-fontset-font
+   nil '(#x3040 . #x30ff)
+   (font-spec :family "NfMotoyaCedar"))
   ;; フォントの横幅を調節する
   (setq face-font-rescale-alist
         '((".*Menlo.*" . 1.0)
           (".*Hiragino_Mincho_Pro.*" . 1.2)
-;;          (".*nfmotoyacedar-bold.*" . 1.2)
-;;          (".*nfmotoyacedar-medium.*" . 1.2)
+          (".*nfmotoyacedar-bold.*" . 1.2)
+          (".*nfmotoyacedar-medium.*" . 1.2)
           ("-cdac$" . 1.3))))
 
 (when (eq system-type 'windows-nt)
   ;; asciiフォントをConsolasに
   (set-face-attribute 'default nil
                       :family "Consolas"
-                      :height 120)
+                      :height 110)
   ;; 日本語フォントをメイリオに
   (set-fontset-font
    nil
@@ -195,28 +194,206 @@
         '((".*Consolas.*" . 1.0)
           (".*メイリオ.*" . 1.15)
           ("-cdac$" . 1.3))))
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; 5.7 ハイライトの設定                                   ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; P100 現在行のハイライト
+(defface my-hl-line-face
+  ;; 背景がdarkならば背景色を紺に
+  '((((class color) (background dark))
+     (:background "NavyBlue" t))
+    ;; 背景がlightならば背景色を緑に
+    (((class color) (background light))
+     (:background "LightGoldenrodYellow" t))
+    (t (:bold t)))
+  "hl-line's my face")
+(setq hl-line-face 'my-hl-line-face)
+(global-hl-line-mode t)
+
+;; P101 括弧の対応関係のハイライト
+;; paren-mode：対応する括弧を強調して表示する
+(setq show-paren-delay 0) ; 表示までの秒数。初期値は0.125
+(show-paren-mode t) ; 有効化
+;; parenのスタイル: expressionは括弧内も強調表示
+(setq show-paren-style 'expression)
+;; フェイスを変更する
+(set-face-background 'show-paren-match-face nil)
+(set-face-underline-p 'show-paren-match-face "yellow")
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; 5.8 バックアップとオートセーブ                         ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; P102-103 バックアップとオートセーブの設定
+;; バックアップファイルを作成しない
+;; (setq make-backup-files nil) ; 初期値はt
+;; オートセーブファイルを作らない
+;; (setq auto-save-default nil) ; 初期値はt
+
+;; バックアップファイルの作成場所をシステムのTempディレクトリに変更する
+;; (setq backup-directory-alist
+;;       `((".*" . ,temporary-file-directory)))
+;; オートセーブファイルの作成場所をシステムのTempディレクトリに変更する
+;; (setq auto-save-file-name-transforms
+;;       `((".*" ,temporary-file-directory t)))
+
+;; バックアップとオートセーブファイルを~/.emacs.d/backups/へ集める
+(add-to-list 'backup-directory-alist
+             (cons "." "~/.emacs.d/backups/"))
+(setq auto-save-file-name-transforms
+      `((".*" ,(expand-file-name "~/.emacs.d/backups/") t)))
+
+;; オートセーブファイル作成までの秒間隔
+(setq auto-save-timeout 15)
+;; オートセーブファイル作成までのタイプ間隔
+(setq auto-save-interval 60)
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; 5.9 フック                                             ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; ファイルが #! から始まる場合、+xを付けて保存する
+(add-hook 'after-save-hook
+          'executable-make-buffer-file-executable-if-script-p)
+
+;; emacs-lisp-mode-hook用の関数を定義
+(defun elisp-mode-hooks ()
+  "lisp-mode-hooks"
+  (when (require 'eldoc nil t)
+    (setq eldoc-idle-delay 0.2)
+    (setq eldoc-echo-area-use-multiline-p t)
+    (turn-on-eldoc-mode)))
+
+;; emacs-lisp-modeのフックをセット
+(add-hook 'emacs-lisp-mode-hook 'elisp-mode-hooks)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; 6.1 Elispをインストールしよう                          ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; ▼要拡張機能インストール▼
+;;; P113 拡張機能を自動インストール──auto-install
+;; auto-installの設定
+(when (require 'auto-install nil t)	; ←1●
+  ;; 2●インストールディレクトリを設定する 初期値は ~/.emacs.d/auto-install/
+  (setq auto-install-directory "~/.emacs.d/elisp/")
+  ;; EmacsWikiに登録されているelisp の名前を取得する
+  (auto-install-update-emacswiki-package-name t)
+  ;; 必要であればプロキシの設定を行う
+  ;; (setq url-proxy-services '(("http" . "localhost:8339")))
+  ;; 3●install-elisp の関数を利用可能にする
+  (auto-install-compatibility-setup)) ; 4●
+
+;; ▼要拡張機能インストール▼
+;;; P114-115 auto-installを利用する
+;; (install-elisp "http://www.emacswiki.org/emacs/download/redo+.el")
+;;(when (require 'redo+ nil t)
+  ;; C-' にリドゥを割り当てる→できない
+  ;;(global-set-key (kbd "C-'") 'redo)
+  ;; 日本語キーボードの場合C-. などがよいかも
+;;   (global-set-key (kbd "C-.") 'redo)
+;;   ) ; ←ここでC-x C-eで設定反映
+
+;;Marmaladeにはanythingが登録されている→なかった　新しいhelmというやつをインストール
+(require 'package)
+(add-to-list 'package-archives
+    '("marmalade" .
+      "http://marmalade-repo.org/packages/"))
+(package-initialize)
+;;
+;;helm
+;;
+(require 'helm-config)
+(helm-mode 1)
+
+(global-set-key (kbd "M-x") 'helm-M-x)
+(require 'helm-descbinds)
+(helm-descbinds-mode)
+
+;;
+;; Auto Complete
+;;http://keisanbutsuriya.hateblo.jp/entry/2015/02/08/175005より
+(require 'auto-complete-config)
+(ac-config-default)
+(add-to-list 'ac-modes 'text-mode)         ;; text-modeでも自動的に有効にする
+(add-to-list 'ac-modes 'fundamental-mode)  ;; fundamental-mode
+(add-to-list 'ac-modes 'org-mode)
+(add-to-list 'ac-modes 'yatex-mode)
+(ac-set-trigger-key "TAB")
+(setq ac-use-menu-map t)       ;; 補完メニュー表示時にC-n/C-pで補完候補選択
+(setq ac-use-fuzzy t)          ;; 曖昧マッチ
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; 6.4 検索と置換の拡張                                   ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; ▼要拡張機能インストール▼
+;;; P132 検索結果をリストアップする──color-moccur
+;;https://www.emacswiki.org/emacs/download/color-moccur.el
+;; color-moccurの設定
+(when (require 'color-moccur nil t)
+  ;; M-oにoccur-by-moccurを割り当て
+  (define-key global-map (kbd "M-o") 'occur-by-moccur)
+  ;; スペース区切りでAND検索
+  (setq moccur-split-word t)
+  ;; ディレクトリ検索のとき除外するファイル
+  (add-to-list 'dmoccur-exclusion-mask "\\.DS_Store")
+  (add-to-list 'dmoccur-exclusion-mask "^#.+#$")
+  ;; Migemoを利用できる環境であればMigemoを使う
+  (when (and (executable-find "cmigemo")
+             (require 'migemo nil t))
+    (setq moccur-use-migemo t)))
+
+;; ▼要拡張機能インストール▼
+;;; P133-134 moccurの結果を直接編集──moccur-edit
+;; moccur-editの設定
+(require 'moccur-edit nil t)
+;; moccur-edit-finish-editと同時にファイルを保存する
+;; (defadvice moccur-edit-change-file
+;;   (after save-after-moccur-edit-buffer activate)
+;;   (save-buffer))
+
+;; ▼要拡張機能インストール▼
+;;; P136 grepの結果を直接編集──wgrep
+;; wgrepの設定
+(require 'wgrep nil t)
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; 6.5 さまざまな履歴管理                                 ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; ▼要拡張機能インストール▼
+;;; P137-138 編集履歴を記憶する──undohist
+;; undohistの設定
+(when (require 'undohist nil t)
+  (undohist-initialize))
+
+;; ▼要拡張機能インストール▼
+;;; P138 アンドゥの分岐履歴──undo-tree
+;; undo-treeの設定
+(when (require 'undo-tree nil t)
+  (global-undo-tree-mode))
 
 
-  (set-face-attribute 'default nil
-                      :family "M+ 1m"
-                      :height 120)
- ;; 日本語フォントをヒラギノ明朝 Proに
-  (set-fontset-font
-   nil 'japanese-jisx0208
-   ;; 英語名の場合
-   ;; (font-spec :family "Hiragino Mincho Pro"))
-   (font-spec :family "M+ 1m"))
-  ;; ひらがなとカタカナをモトヤシーダに
-  ;; U+3000-303F	CJKの記号および句読点
-  ;; U+3040-309F	ひらがな
-;; U+30A0-30FF	カタカナ
-;;  (set-fontset-font
-;;   nil '(#x3040 . #x30ff)
-;;   (font-spec :family "NfMotoyaCedar"))
-  ;; フォントの横幅を調節する
-  (setq face-font-rescale-alist
-        '((".*Menlo.*" . 1.0)
-          (".*Hiragino_Mincho_Pro.*" . 1.2)
-;;          (".*nfmotoyacedar-bold.*" . 1.2)
-;;          (".*nfmotoyacedar-medium.*" . 1.2)
-          ("-cdac$" . 1.3)))
+;; ▼要拡張機能インストール▼
+;;; P139-140 カーソルの移動履歴──point-undo
+;; point-undoの設定
+(when (require 'point-undo nil t)
+  ;; (define-key global-map [f5] 'point-undo)
+  ;; (define-key global-map [f6] 'point-redo)
+  ;; 筆者のお勧めキーバインド
+  (define-key global-map (kbd "M-[") 'point-undo)
+  (define-key global-map (kbd "M-]") 'point-redo)
+  )
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; 6.6 ウィンドウ管理                                     ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; ▼要拡張機能インストール▼
+;;; P141-143 ウィンドウの分割状態を管理──ElScreen
+;; ElScreenのプレフィックスキーを変更する（初期値はC-z）
+ (setq elscreen-prefix-key (kbd "C-t"))
+(when (require 'elscreen nil t)
+  ;; C-z C-zをタイプした場合にデフォルトのC-zを利用する
+  (if window-system
+      (define-key elscreen-map (kbd "C-z") 'iconify-or-deiconify-frame)
+    (define-key elscreen-map (kbd "C-z") 'suspend-emacs)))
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; 6.8 特殊な範囲の編集                                   ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; P151 矩形編集──cua-mode
+;; cua-modeの設定
+(cua-mode t) ; cua-modeをオン
+(setq cua-enable-cua-keys nil) ; CUAキーバインドを無効にする
